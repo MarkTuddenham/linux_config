f178e9fc736b4e79977b033c97abf466aa4eb755
Mark Tuddenham <mark@tudders.com>
2021-07-25 13:50:57 +0100

    Telescope layout settings

--- nvim/.config/nvim/lua/tudders/telescope.lua
+++ nvim/.config/nvim/lua/tudders/telescope.lua
@@ -1,11 +1,21 @@
--- telescope fzf setup
 require('telescope').setup {
   defaults = {
     color_devicons = true,
     layout_config = {
+      prompt_position = 'top',
       horizontal = {
-        prompt_position = 'top',
+        preview_width = function(_, cols, _)
+          if cols > 200 then
+            return math.floor(cols * 0.4)
+          else
+            return math.floor(cols * 0.6)
+          end
+        end,
       },
+    },
+    sorting_strategy = 'ascending',
+    file_ignore_patterns = {
+      '.git/*', -- we have set hidden files to shown, but we don't want git dotfiles
     },
   },
   extensions = {
@@ -13,15 +23,14 @@ require('telescope').setup {
       fuzzy = true,                    -- false will only do exact matching
       override_generic_sorter = false, -- override the generic sorter
       override_file_sorter = true,     -- override the file sorter
-      case_mode = 'smart_case',        -- or 'ignore_case' or 'respect_case'
-                                       -- the default case_mode is 'smart_case'
+      case_mode = 'smart_case',        -- 'smart_case' or 'ignore_case' or 'respect_case'
     }
   }
 }
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
 require('telescope').load_extension('fzf')
-require("telescope").load_extension("git_worktree")
+require('telescope').load_extension('git_worktree')
 
 local opts = {noremap = true, silent = true}
 vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>lua require("telescope.builtin").find_files({hidden=true})<cr>', opts)
