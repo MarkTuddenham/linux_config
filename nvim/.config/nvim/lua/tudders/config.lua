6983bfdf1acfbf7d5e454e5e6c0ef2445570be01
Mark Tuddenham <mark@tudders.com>
2021-07-28 23:13:10 +0100

    Extract status line into its own config file

--- nvim/.config/nvim/lua/tudders/config.lua
+++ nvim/.config/nvim/lua/tudders/config.lua
@@ -1,5 +1,6 @@
-require 'tudders.opts'
-require 'tudders.telescope'
+require('tudders.opts')
+require('tudders.telescope')
+require('tudders.statusline')
 
 require('which-key').setup()
 
@@ -172,91 +173,3 @@ vim.api.nvim_set_keymap('s', '<Tab>', 'v:lua.tab_complete()', {expr = true})
 vim.api.nvim_set_keymap('i', '<S-Tab>', 'v:lua.s_tab_complete()', {expr = true})
 vim.api.nvim_set_keymap('s', '<S-Tab>', 'v:lua.s_tab_complete()', {expr = true})
 
-
---lspconfig
-local nvim_lsp = require('lspconfig')
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-  --Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  local lsp_opts = { noremap=true, silent=true }
-
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', lsp_opts)
-  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', lsp_opts)
-  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', lsp_opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', lsp_opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>', lsp_opts)
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>', lsp_opts)
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>', lsp_opts)
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>', lsp_opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<cr>', lsp_opts)
-  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', lsp_opts)
-  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', lsp_opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', lsp_opts)
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<cr>', lsp_opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>', lsp_opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<cr>', lsp_opts)
-  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>', lsp_opts)
-  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<cr>', lsp_opts)
-
-end
-
--- LSP: Others
--- Use a loop to conveniently call 'setup' on multiple servers and
--- map buffer local keybindings when the language server attaches
-local servers = { 'clangd', 'texlab'}
-for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup{
-    on_attach = on_attach,
-    flags = {
-      debounce_text_changes = 150,
-    }
-  }
-end
-
--- LSP: Python
-nvim_lsp.pylsp.setup{
-  on_attach = on_attach,
-  flags = {
-    debounce_text_changes = 150,
-  },
-  plugins = {
-    pydocstyle = {enabled = true},
-    mypy = {enabled = true, config_file='/home/mark/.config/mypy/config'},
-  },
-}
--- LSP: Lua
-USER = vim.fn.expand('$USER')
-
-local sumneko_binary = '/home/' .. USER .. '/build/lua-language-server/bin/Linux/lua-language-server'
-local sumneko_root_path = '/home/' .. USER .. '/build/lua-language-server'
-
-require('lspconfig').sumneko_lua.setup {
-  on_attach = on_attach,
-  flags = {
-    debounce_text_changes = 150,
-  },
-  cmd = {sumneko_binary, '-E', sumneko_root_path .. '/main.lua'},
-  settings = {
-    Lua = {
-      runtime = {
-        version = 'LuaJIT',
-        path = vim.split(package.path, ';')
-      },
-      diagnostics = {
-        globals = {'vim'}
-      },
-      workspace = {
-        library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true}
-      }
-    }
-  }
-}
