432b16667d58d3c226e307190a4d43e567acb498
Mark Tuddenham <mark@tudders.com>
2021-09-03 14:06:54 +0100

    Change leader key, clean init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,27 +4,12 @@ end
 
 require('tudders.plugins')
 
--- Leader key -> ","
+-- Leader key -> " "
 --
 -- In general, it's a good idea to set this early in your config, because otherwise
 -- if you have any mappings you set BEFORE doing this, they will be set to the OLD
 -- leader.
-vim.g.mapleader = ","
+-- vim.api.nvim_set_keymap('n', '<space>', '<nop>', {})  -- do we have to unmap space first?
+vim.g.mapleader = ' '
 
 require('tudders.config')
--- require('tudders.statusline')
-
-
-vim.g.vim_markdown_math = 1
-
--- autocmd BufWritePre *.c lua vim.lsp.buf.formatting_sync(nil, 1000)
--- autocmd BufWritePre *.cpp lua vim.lsp.buf.formatting_sync(nil, 1000)
-
-vim.g.cpp_class_scope_highlight = 1
-vim.g.cpp_class_scope_highlight = 1
-vim.g.cpp_class_decl_highlight = 1
-vim.g.cpp_member_variable_highlight = 1
-vim.g.cpp_experimental_simple_template_highlight = 1
-
-
-vim.cmd('source $HOME/.config/nvim/keybindings.vim')
--- nvim/.config/nvim/lua/tudders/opts.lua
+++ nvim/.config/nvim/lua/tudders/opts.lua
@@ -93,4 +93,18 @@ vim.g.HardMode_level = 'wannabe'
 vim.g.HardMode_hardmodeMsg = 'Don\'t use this!'
 vim.cmd('au VimEnter,BufNewFile,BufReadPost * silent! call HardMode()')
 
-vim.g.camelcasemotion_key = '<leader>'
+vim.g.camelcasemotion_key = ',' -- '<leader>'
+
+vim.g.vim_markdown_math = 1
+
+-- autocmd BufWritePre *.c lua vim.lsp.buf.formatting_sync(nil, 1000)
+-- autocmd BufWritePre *.cpp lua vim.lsp.buf.formatting_sync(nil, 1000)
+
+vim.g.cpp_class_scope_highlight = 1
+vim.g.cpp_class_scope_highlight = 1
+vim.g.cpp_class_decl_highlight = 1
+vim.g.cpp_member_variable_highlight = 1
+vim.g.cpp_experimental_simple_template_highlight = 1
+
+
+vim.cmd('source $HOME/.config/nvim/keybindings.vim')
