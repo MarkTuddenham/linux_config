b146898bd3d104e4a5716982a83cbaa0e45169fc
Mark Tuddenham <mark@tudders.com>
2021-09-22 19:27:13 +0100

    feat: remove redundant spaceship config

--- zsh/.config/zsh/include/spaceship.zsh
+++ zsh/.config/zsh/include/spaceship.zsh
@@ -1,13 +1,39 @@
-export SPACESHIP_PROMPT_DEFAULT_PREFIX=' > '
+# # Prefix
+# if [ $IS_SSH ]; then
+#   # Prompt replacement symbols
+#   export SPACESHIP_PROMPT_DEFAULT_PREFIX='/ '
+#   export SPACESHIP_CHAR_SYMBOL='-> '
+
+#   # Git replacement symbols
+#   export SPACESHIP_GIT_SYMBOL='git:'
+
+#   export SPACESHIP_GIT_STATUS_UNTRACKED='?'
+#   export SPACESHIP_GIT_STATUS_AHEAD='^'
+#   export SPACESHIP_GIT_STATUS_BEHIND='v'
+#   export SPACESHIP_GIT_STATUS_DIVERGED='<diverged>'
+
+#   # Pyenv
+#   SPACESHIP_PYENV_SYMBOL="pyenv:"
+# else
+#   export SPACESHIP_PROMPT_DEFAULT_PREFIX=' '
+# fi
+
+export SPACESHIP_PROMPT_DEFAULT_PREFIX=' ❯ '
 export SPACESHIP_PROMPT_ADD_NEWLINE=false
-export SPACESHIP_USER_SHOW=always
-export SPACESHIP_HOST_SHOW=always
+export SPACESHIP_USER_SHOW=needed
+export SPACESHIP_HOST_SHOW=true
 export SPACESHIP_EXIT_CODE_SHOW=true
 
 # Time
 export SPACESHIP_TIME_SHOW=true
 
+export SPACESHIP_VI_MODE_SHOW=true
+export SPACESHIP_DIR_TRUNC_REPO=false
+export SPACESHIP_DIR_TRUNC_PREFIX='...'
+
+
 SPACESHIP_PROMPT_ORDER=(
+	vi_mode
   user
   host
   dir
@@ -21,7 +47,6 @@ SPACESHIP_PROMPT_ORDER=(
   kubectl
   exec_time
   line_sep
-  vi_mode
   jobs
   exit_code
   char
